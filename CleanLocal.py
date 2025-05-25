2bbc0d7b12e1ba0c2547525e63512408f76c4826
Rochak Wadhwa <rochak.wadhwa@gmail.com>
2025-05-25 12:56:22 +0530

    Update CleanLocal.py

--- CleanLocal.py
+++ CleanLocal.py
@@ -1,37 +1,23 @@
 # ============================================================
-
 # Author: Rochak Wadhwa
-
-# Team: Data Engineering under Vinod Boga
-
 # Purpose of this sctipt: Clean logs and not needed old files from local of HDP node
-
 # This script can be executed using root user
-
 # Version - Oct 16
-
 # ============================================================
 
 
 
 # ============================================================
-
 # importing modules and utilities
-
 import os
-
 import time
-
 import getpass as gt
-
 import stat
 
 
 
 # ============================================================
-
 # List for deletion of log files ending like out.1 or log.7
-
 loc01 = "/var/log/ambari-agent/"
 
 loc02 = "/var/log/ambari-infra-solr/"
